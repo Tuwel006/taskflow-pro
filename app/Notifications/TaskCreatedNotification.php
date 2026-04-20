99d2a92be1e823b0c4961d616e21741639a2da1e
Tuwel Shaikh <tuwelshaikh006@gmail.com>
2026-04-21 01:21:58 +0530

    feat: implement ShouldQueue in TaskCreatedNotification to enable background processing

--- app/Notifications/TaskCreatedNotification.php
+++ app/Notifications/TaskCreatedNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TaskCreatedNotification extends Notification
+class TaskCreatedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
